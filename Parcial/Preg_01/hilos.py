12ec42414f46395dad2f431334a26eee18a2d6ae
yeyson-cano <125895601+yeyson-cano@users.noreply.github.com>
2023-10-07 12:07:06 -0500

    Update and rename preg_01.py to hilos.py

--- Parcial/Preg_01/hilos.py
+++ Parcial/Preg_01/hilos.py
@@ -4,9 +4,9 @@ import threading
 import requests
 from io import BytesIO
 
-def aplicar_filtro(img, kernel, resultado, indice):
+def aplicar_filtro(img, kernel):
     img_filtrada = cv2.filter2D(img, -1, kernel)
-    resultado[indice] = img_filtrada
+    return img_filtrada
 
 def guardar_imagen(nombre_archivo, img):
     cv2.imwrite(nombre_archivo, img)
@@ -35,46 +35,39 @@ def main():
                               [-1, 8, -1],
                               [-1, -1, -1]])
 
-    num_hilos = 4
-    alto, ancho, _ = img.shape
-    alto_parte = alto // num_hilos
-    partes = []
+    num_hilos = 4  # Puedes ajustar el número de hilos según tus necesidades
     resultados_suavizado = [None] * num_hilos
     resultados_bordes = [None] * num_hilos
-
-    for i in range(num_hilos):
-        fila_inicio = i * alto_parte
-        fila_fin = (i + 1) * alto_parte if i < num_hilos - 1 else alto
-        parte = img[fila_inicio:fila_fin, :, :]
-        partes.append(parte)
 
     hilos_suavizado = []
     hilos_bordes = []
 
     for i in range(num_hilos):
-        hilo_suavizado = threading.Thread(target=aplicar_filtro, args=(partes[i], kernel_suavizado, resultados_suavizado, i))
-        hilo_bordes = threading.Thread(target=aplicar_filtro, args=(partes[i], kernel_bordes, resultados_bordes, i))
+        hilo_suavizado = threading.Thread(target=lambda i=i: resultados_suavizado.__setitem__(i, aplicar_filtro(img, kernel_suavizado)))
+        hilo_bordes = threading.Thread(target=lambda i=i: resultados_bordes.__setitem__(i, aplicar_filtro(img, kernel_bordes)))
         hilos_suavizado.append(hilo_suavizado)
         hilos_bordes.append(hilo_bordes)
 
     for hilo in hilos_suavizado:
         hilo.start()
 
+    for hilo in hilos_bordes:
+        hilo.start()
+
     for hilo in hilos_suavizado:
         hilo.join()
 
     for hilo in hilos_bordes:
-        hilo.start()
-
-    for hilo in hilos_bordes:
         hilo.join()
 
-    img_filtrada_suavizado = np.vstack(resultados_suavizado)
-    img_filtrada_bordes = np.vstack(resultados_bordes)
+    img_filtrada_suavizado = resultados_suavizado[0]
+    img_filtrada_bordes = resultados_bordes[0]
 
     # Guardar las imágenes filtradas como archivos JPEG
-    guardar_imagen("imagen_suavizada.jpg", img_filtrada_suavizado)
-    guardar_imagen("imagen_con_bordes.jpg", img_filtrada_bordes)
+    if img_filtrada_suavizado is not None:
+        guardar_imagen("imagen_suavizada_hilos.jpg", img_filtrada_suavizado)
+    if img_filtrada_bordes is not None:
+        guardar_imagen("imagen_con_bordes_hilos.jpg", img_filtrada_bordes)
 
 if __name__ == "__main__":
     main()
